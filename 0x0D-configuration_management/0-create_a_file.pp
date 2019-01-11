df4029d2c6689b219e2819e66aa9f547876e3d3b
leobyeon <386@holbertonschool.com>
2019-01-11 01:16:59 +0000

    Changed filename

--- 0x0D-configuration_management/0-create_a_file.pp
+++ 0x0D-configuration_management/0-create_a_file.pp
@@ -1,8 +1,6 @@
 # creates a file in /tmp
-file { '/tmp':
-  ensure  => 'file',
+file { '/tmp/holberton':
   mode    => '0744',
-  path    => '/tmp/holberton',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
