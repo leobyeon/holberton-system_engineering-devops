23b99941e300d0a6d63204ad18c4dbd99a1e0e38
leobyeon <386@holbertonschool.com>
2019-02-13 22:28:37 +0000

    readding 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,13 +3,13 @@
 queries the Reddit API and prints the titles
 of the first 10 hot posts listed for a given subreddit
 """
+import requests
 
 
 def top_ten(subreddit):
     """
     print the titles of the 10 hot posts
     """
-    import requests
 
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {
